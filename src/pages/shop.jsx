65d0344fd5e4ac9616105db7e0cabf847b52bfb6
Bamboo-rat <135868167+Bamboo-rat@users.noreply.github.com>
2024-06-21 03:50:09 +0700

    Update shop.jsx

--- src/pages/shop.jsx
+++ src/pages/shop.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../assets/css/shop.css'
+import '../assets/css/shop.css';
 import anh1 from '../assets/images/anh1.jpg';
 import anh2 from '../assets/images/anh2.jpg';
 import anh3 from '../assets/images/anh3.jpg';
@@ -7,6 +7,11 @@ import anh4 from '../assets/images/anh4.jpg';
 import anh5 from '../assets/images/anh5.jpg';
 import anh6 from '../assets/images/anh6.jpg';
 import anh7 from '../assets/images/anh7.jpg';
+import logo from '../assets/images/logo.png';
+import Navbar from '../component/navbar';
+import TestimonialChild from '../component/testimonial-child';
+import Footer from '../component/footer';
+import ImageSlider from '../component/Slide';
 
 const services = [
   { img: anh1, title: 'Hair Cut', price: '$10.00' },
@@ -21,29 +26,33 @@ const services = [
 
 function Shop() {
   return (
-    <div className="ctn11">
-      <div className="ctn1 container">
-        {services.map((service, index) => (
-          <div className="row" key={index}>
-            <div className="col">
-              <div className="anh1">
-                <img src={service.img} alt="" className="shop" />
-                <div className="haircut">
-                  <h1>
-                    <div className="haircut2">
-                      {service.title}
-                      <div className="anh1-chu">........................................................</div>
-                      <div className={`anh1-chu${index + 1}`}>{service.price}</div>
-                    </div>
-                    <p>Their days lesser and every firmament</p>
-                  </h1>
-                </div>
+    <div>
+      <Navbar />
+     <ImageSlider/>
+      <div className="logo-d">
+        <img src={logo} alt="Logo" className="anhlogo" />
+        <div className="chu-d">
+          <p>Good morning forth gathering doesn't god gathering man and had moveth there dry sixth dominion</p>
+          <p>Rule divided behold after had he did not move.</p>
+        </div>
+      </div>
+      
+      <div className="shop-container">
+        <div className="services-container">
+          {services.map((service, index) => (
+            <div className="service-item" key={index}>
+              <img src={service.img} alt={service.title} className="service-image" />
+              <div className="service-details">
+                <h1>{service.title}</h1>
+                <div className="service-price">{service.price}</div>
+                <p>Their days lesser and every firmament</p>
               </div>
             </div>
-            {index % 2 === 1 && <div className="cot"></div>}
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
+      <TestimonialChild />
+      <Footer />
     </div>
   );
 }
