fec43c645eb2855a35f4cd99f60410bfaf93e7d6
Bamboo-rat <135868167+Bamboo-rat@users.noreply.github.com>
2024-06-21 03:49:21 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import './assets/css/reset.css'
 import './assets/css/web.css'
@@ -6,13 +7,23 @@ import Navbar from './component/navbar';
 import Service from './pages/service';
 import Features from './pages/features';
 import Shop from './pages/shop';
+import Meetus from './pages/meetus';
+import Banner from './pages/banner';
+import Testimonial from './pages/testimonial';
 
 function App() {
   return (
     <div className="App">
-      {/* <Service /> */}
-      {/* <Features /> */}
-      <Shop />
+      <div className="App">
+          <Routes>
+            <Route path="/" element={<Features />} />
+            <Route path="/features" element={<Banner />} />
+            <Route path="/meetus" element={<Meetus />} />
+            <Route path="/service" element={<Service />} />
+            <Route path="/shop" element={<Shop />} />
+            <Route path="/testimonial" element={<Testimonial />} />
+          </Routes>
+        </div>
     </div>
   );
 }
