cb2c5eb07ffd296e5640d49b21ffe4408f551965
Bamboo-rat <135868167+Bamboo-rat@users.noreply.github.com>
2024-06-21 03:50:33 +0700

    Update service.jsx

--- src/pages/service.jsx
+++ src/pages/service.jsx
@@ -4,10 +4,19 @@ import trim from '../assets/images/trim.jpg';
 import style from '../assets/images/style.jpg';
 import shampoo from '../assets/images/shampoo.jpg';
 import cut from '../assets/images/cut.jpg';
+import Navbar from '../component/navbar';
+import TestimonialChild from '../component/testimonial-child';
+import Footer from '../component/footer';
+import service1 from "../assets/images/service-icon-1.png"
+import service2 from "../assets/images/service-icon-2.png"
+import service3 from "../assets/images/service-icon-3.png"
+import Carousel from '../component/Carousel';
 
 function Service() {
   return (
-    <div><section id="service">
+    <div>
+        <Navbar />
+        <section id="service">
     <div class="title-text">
         <p>SERVICES</p>
         <h1>We Provider Better</h1>
@@ -58,7 +67,53 @@ function Service() {
             </div>
         </div>
     </div>
-</section></div>
+</section>
+<Carousel/>
+<div className="space-medium">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+              <div className="service-block">
+                <div className="service-icon mb20">
+                  <img src={service1} alt="Traditional Cut" />
+                </div>
+                <div className="service-content">
+                  <h2><a href="#" className="title">Traditional Cut</a></h2>
+                  <p>Get a traditional cut with professional styling to make you look your best.</p>
+                  <div className="price">$45</div>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+              <div className="service-block">
+                <div className="service-icon mb20">
+                  <img src={service2} alt="Mustache Trim" />
+                </div>
+                <div className="service-content">
+                  <h2><a href="#" className="title">Mustache Trim</a></h2>
+                  <p>Keep your mustache looking sharp and well-groomed with our trim service.</p>
+                  <div className="price">$45</div>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+              <div className="service-block">
+                <div className="service-icon mb20">
+                  <img src={service3} alt="Beard Trim" />
+                </div>
+                <div className="service-content">
+                  <h2><a href="#" className="title">Beard Trim</a></h2>
+                  <p>Get a professionally styled beard trim to enhance your look.</p>
+                  <div className="price">$45</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+<TestimonialChild/>
+<Footer/>
+</div>
   )
 }
 
